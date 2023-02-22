ed0160d3234cf21072889f644fa21e8bd6698b71
Whitney Muchira <whitneymuchira@gmail.com>
2023-02-22 22:58:05 +0300

    sum total function

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ let billInput=document.querySelector('#cash-bill')
 let percentages=document.querySelectorAll('.percentages')
 let tip=document.getElementById('total-amount')
 let totalPerPerson=document.querySelector('#total-person')
+const persons=document.getElementById('people')
+
 
 function calculateTips(){
 billInput.addEventListener('change', ()=>{
-    let bill=billInput.value
+    let bill=Number(billInput.value)
+   // let person=Number(persons.value)
+//console.log(person)
+console.log(bill)
 
+persons.addEventListener('change', ()=>{
+    let people=Number(persons.value)
+    console.log(people)
+ })
 
     percentages.forEach((percent)=>{
         percent.addEventListener('click',()=>{
@@ -15,17 +24,30 @@ billInput.addEventListener('change', ()=>{
             let number=parseInt(rate)
                     let tipped=bill*number/100
                     console.log(tipped)
+                    
 
                     tip.value=tipped.toFixed(2)
                     
 
-                })
-        })
-        
-  
+                   //calculating total of tips+ bill
+             function sum(){
+            total=Number(tipped+bill)
+            console.log(total)
 
+            //finding each cost
+             
+
+             }
+
+             
+             sum()
+              
+                })
+                
+        })
     })
- 
+
+   
 
 }
 calculateTips()
